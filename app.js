0923cc9f2f5621083d8ea58fc320c66877e16cb6
Naimat Ansari <naimatansari31@gmail.com>
2024-10-12 13:44:53 +0500

    add

--- app.js
+++ app.js
@@ -3,21 +3,30 @@ async function fetchData() {
     const response = await fetch('https://dummyjson.com/users')
     const data = await response.json()
     console.log(data)
+    return data
 }
 
 fetchData().then((userData)=>{
+    console.log(userData);
+    
     let card = document.getElementById('Card')
     userData.users.map((user)=>{
         const profile =`
           <div class="profile-card">
-          <img src= alt=>
-          <h3>Naimat Ali</h3>
-          <p>Email: naimat@example.com</p>
-          <p>Address: Karachi, Pakistan</p>
-          <p>Age: 25</p>
-          <p>Phone: +92 300 1234567</p>
-          <p>Company: Aptech</p>
+          <img src=${user.image}  alt=${user.firstName} >
+          <h3> <b>${user.firstName} </b> </h3>
+          <p>Email: <b>${user.email}</b> </p>
+          <p>Address: <b>${user.address.city}</b>  </p>
+          <p>Age: <b>${user.age}</b>  </p>
+          <p>Gender: <b>${user.gender}</b>  </p>
+          <p>Phone: <b>${user.phone}</b>  </p>
+          <p>Designation: <b>${user.company.title}</b>  </p>
+           <a href="user.html?id=${user.id}" class="view-profile-btn">View Profile</a>
         </div>
         `
+
+        card.innerHTML += profile
     })
+}).catch((err)=>{
+    console.log(err)
 })
